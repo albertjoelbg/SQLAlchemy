8a3644a71e5cddec9f9f81322c26fffed57e1705
Albert Joel Bonilla Guerrero <albertjoelbg@gmail.com>
2023-10-10 11:13:31 -0500

    deploy vercel

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-from fastapi import FastAPI
+import uvicorn
+from os import getenv
 
-app = FastAPI()
-
-
-@app.get("/hello/{name}", response_model=dict)
-async def hello(name: str):
-    """
-    Saluda a la persona con el nombre proporcionado.
-
-    Args:
-        name (str): El nombre de la persona a saludar.
-
-    Returns:
-        dict: Un diccionario que contiene el nombre saludo.
-    """
-    return {
-        "name": name
-    }
+if __name__ == "__main__":
+    port = int(getenv("PORT", 8000))
+    uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=True)
